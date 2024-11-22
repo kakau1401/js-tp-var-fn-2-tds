b57460efcf99ffe537c8aa0971da0da525f9f825
kakau1401 <160244211+kakau1401@users.noreply.github.com>
2024-11-22 08:33:37 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operaçãoMatematica= primeiroNumero-segundoNumero;
+
+console.log (operaçãoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperação = primeiroNumero/numeroPontoFlutuante;
+
+console.log(novaOperação)
+
+//NaN -> Not a number (não é um numero)
+
+const alura = ("Alura");
+console.log(alura*primeiroNumero)
